ea768c38f0fe8ce076ba455c432b65fb3e8e67df
AbuzarAslam <120781089+AbuzarAslam@users.noreply.github.com>
2023-01-02 07:07:00 -0800

    Add files via upload

---
+++ task5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void greater(int number1, int number2);
+main()
+{
+int number1,number2;
+cout << "Enter 1st number....";
+cin >> number1;
+cout << "Enter 2nd number....";
+cin >> number2;
+void greater(number1, number2);
+}
+void greater(int number1, int number2)
+ {
+	if(number1 > number2)
+	{
+	cout "Greater number is...." << number1 << endl;
+	}
+	if(number2 > number1)
+	{
+	cout "Greater number is...." << number2 << endl;
+	}
+ }
